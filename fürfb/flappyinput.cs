eb868df3a9b586aaf939f36bafff9e211df57c69
Stührk <hanna.stuehrk@haw-hamburg.de>
2016-10-07 16:11:16 +0200

    csdaten für flappy aufgabe

---
+++ fürfb/flappyinput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.SceneManagement;
+
+public class flappyinput : MonoBehaviour {
+
+	public float jumpforce = 40f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		detectInput();
+
+	
+	}
+	void detectInput(){
+		if (Input.GetKey ("space")) {
+			Debug.Log ("ButtonPressed");
+			Jump ();
+		}
+	}
+
+	void Jump(){
+		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0f, 0f, 0f);
+		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f,jumpforce,0f));
+			
+			}
+	void OnCollisionEnter(){
+		Debug.Log ("wat");
+		SceneManager.LoadScene (0);
+	}
+	//instanciate
+	//mathf.random
+			//JetBrains
+}
